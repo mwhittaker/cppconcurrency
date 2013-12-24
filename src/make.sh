a8553ff9ce06841de5103a951f04bcbfc0a84db1
Michael Whittaker <mjw297@cornell.edu>
2013-12-24 14:24:34 -0800

    Improved Makefile to auto-build cpp's.
    Now, the makefile will build all .cpp files in the directory. make.sh
    was removed as it is no longer necessary.

--- src/make.sh
+++
@@ -1,8 +0,0 @@
-if test $# -lt 1; then
-    echo "Please enter the name of a .cpp file."
-    exit 1
-fi
-
-CPPFLAGS="-Wall -Wextra -Werror -g -O2 -Wall -pedantic -std=c++11 -pthread"
-
-g++ $CPPFLAGS "$1.cpp" -o "$1.out"
